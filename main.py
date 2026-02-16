991a33803e77cb9a12e0981c9369e80affa44ace
Georgia Rawson <georgiaisabellawatson@gmail.com>
2026-02-16 18:34:59 +0000

    added list to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from InquirerPy import prompts
+from InquirerPy import prompt
+
 
 questions = [
     {"type": "input", "name": "title", "message": "Enter your project title:"},
@@ -7,6 +8,7 @@ questions = [
     {"type": "input", "name": "useage", "message": "Enter useage information:"},
     {"type": "input", "name": "author", "message": "Authors name:"},
     {"type": "input", "name": "contact", "message": "Contact information:"},
-   
+    {"type": "list", "name": "licence", "message": "Select a licence:", "choices": ["Apache License 2,0", "MIT License", "GNU GPL v3", "GNU LGPL v3", "Mozilla Public License 2.0", "Creative Commons" , "Unlicense",], "default": None,}
 ]
+
 answers = prompt(questions)
