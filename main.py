e34dcf73d412de3859fde8da1333367e7456a77c
Georgia Rawson <georgiaisabellawatson@gmail.com>
2026-02-17 13:26:18 +0000

    added readme variable and file creation code

--- main.py
+++ main.py
@@ -10,5 +10,32 @@ questions = [
     {"type": "input", "name": "contact", "message": "Contact information:"},
     {"type": "list", "name": "licence", "message": "Select a licence:", "choices": ["Apache License 2,0", "MIT License", "GNU GPL v3", "GNU LGPL v3", "Mozilla Public License 2.0", "Creative Commons" , "Unlicense",], "default": None,}
 ]
-
 answers = prompt(questions)
+
+content = f"""
+           # {answers['title']}
+           
+           ## Description
+
+           {answers['description']}
+            
+           ## Installation 
+
+           {answers['installation']} 
+   
+           ## Useage 
+           
+           {answers['useage']}
+           
+           ## Author 
+           
+           {answers['author']} <{answers ['contact']}>
+           
+           ## Licence
+           
+           
+           {answers}['licence']"""
+
+          
+with open ("README.md", "w") as readme:
+    readme.write(content)
