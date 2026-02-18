05026d1b143aa9ed62eddd9fb37cb548ae6fc301
Georgia Rawson <georgiaisabellawatson@gmail.com>
2026-02-18 17:53:12 +0000

    adding file system

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from InquirerPy import prompt
+import questions
 
-
-questions = [
-    {"type": "input", "name": "title", "message": "Enter your project title:"},
-    {"type": "input", "name": "description", "message": "Describe your project:"},
-    {"type": "input", "name": "installation", "message": "Enter Installation instructions:"},
-    {"type": "input", "name": "useage", "message": "Enter useage information:"},
-    {"type": "input", "name": "author", "message": "Authors name:"},
-    {"type": "input", "name": "contact", "message": "Contact information:"},
-    {"type": "list", "name": "licence", "message": "Select a licence:", "choices": ["Apache License 2,0", "MIT License", "GNU GPL v3", "GNU LGPL v3", "Mozilla Public License 2.0", "Creative Commons" , "Unlicense",], "default": None,}
-]
 answers = prompt(questions)
 
 content = f"""
@@ -33,9 +24,10 @@ content = f"""
            
            ## Licence
            
-           
            {answers['licence']}"""
 
           
 with open ("README.md", "w") as readme:
     readme.write(content)
+
+print("README.md has been generated sucessfully!")
---
+++ questions.py
@@ -0,0 +1,11 @@
+from InquirerPy import prompt
+questions = [
+    {"type": "input", "name": "title", "message": "Enter your project title:"},
+    {"type": "input", "name": "description", "message": "Describe your project:"},
+    {"type": "input", "name": "installation", "message": "Enter Installation instructions:"},
+    {"type": "input", "name": "useage", "message": "Enter useage information:"},
+    {"type": "input", "name": "author", "message": "Authors name:"},
+    {"type": "input", "name": "contact", "message": "Contact information:"},
+    {"type": "list", "name": "licence", "message": "Select a licence:", "choices": ["Apache License 2,0", "MIT License", "GNU GPL v3", "GNU LGPL v3", "Mozilla Public License 2.0", "Creative Commons" , "Unlicense",], "default": None,}
+]
+answers = prompt(questions)
